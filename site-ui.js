d629adbc1fe846c5a2ca49c247805e4804de62e7
sinan <muhammadsinanmp5@gmail.com>
2026-03-06 23:57:29 +0530

    first commit

---
+++ site-ui.js
@@ -0,0 +1,50 @@
+﻿(function () {
+  function closeAllDropdowns(exceptMenu) {
+    document.querySelectorAll('.dropdown-menu.show').forEach(function (menu) {
+      if (menu !== exceptMenu) {
+        menu.classList.remove('show');
+      }
+    });
+  }
+
+  document.addEventListener('click', function (event) {
+    var toggle = event.target.closest('[data-ui-toggle="dropdown"]');
+    if (toggle) {
+      event.preventDefault();
+      var menu = toggle.parentElement ? toggle.parentElement.querySelector('.dropdown-menu') : null;
+      if (!menu) {
+        return;
+      }
+
+      var willShow = !menu.classList.contains('show');
+      closeAllDropdowns(menu);
+      if (willShow) {
+        menu.classList.add('show');
+      } else {
+        menu.classList.remove('show');
+      }
+      return;
+    }
+
+    if (!event.target.closest('.dropdown')) {
+      closeAllDropdowns(null);
+    }
+  });
+
+  document.querySelectorAll('.navbar-toggler[data-ui-target]').forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      var targetSelector = btn.getAttribute('data-ui-target');
+      if (!targetSelector) {
+        return;
+      }
+
+      var target = document.querySelector(targetSelector);
+      if (!target) {
+        return;
+      }
+
+      target.classList.toggle('show');
+    });
+  });
+})();
+
